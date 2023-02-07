1cd6de30c23f4ecf905c3e405cda0329d4157bd8
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 15:42:33 -0300

    add new endpoints to response

--- src/routes/base.routes.js
+++ src/routes/base.routes.js
@@ -6,6 +6,10 @@ router.get('/', (_req, res) => {
   res.json({
     message: 'Welcome to the workout API register!',
     endpoints: {
+      authentication: [
+        '/api/register',
+        '/api/login'
+      ],
       version_one: '/api/v1/',
       version_two: '/api/v2/'
     }
