1be452e8aaed75d7ea30f1f76a9a55ead1712e02
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 18:43:20 -0300

    map result to show only data values

--- src/services/tools.services.js
+++ src/services/tools.services.js
@@ -4,6 +4,7 @@ export const findBy = async ({ condition, model, all = false }) => {
   try {
     if (all) {
       result = await model.findAll({ where: condition })
+      result = result.map((data) => data.dataValues)
     } else {
       result = await model.findOne({ where: condition })
     }
