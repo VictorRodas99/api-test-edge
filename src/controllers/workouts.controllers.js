7a53fde0d4555aedd8b2cd3b6b5e12e9c003cdb9
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 18:44:16 -0300

    add controller to get workout by day

--- src/controllers/workouts.controllers.js
+++ src/controllers/workouts.controllers.js
@@ -1,6 +1,6 @@
 import { Workout } from '../models/Workout.js'
 import { findBy } from '../services/tools.services.js'
-import { validateWorkoutFields } from '../utils/validatorWorkout.js'
+import { validateWorkoutFields, parseDay } from '../utils/validatorWorkout.js'
 import { createWorkout as createWorkoutDB } from '../services/workouts.services.js'
 
 export const getWorkouts = async (req, res) => {
@@ -29,7 +29,46 @@ export const getWorkouts = async (req, res) => {
     workouts
   })
 }
-export const getWorkoutByDay = async (req, res) => {}
+
+export const getWorkoutsByDay = async (req, res) => {
+  try {
+    const userId = req.session.id
+    const day = parseDay(req.params.day)
+
+    const workouts = await findBy({
+      condition: { user_id: userId, day },
+      model: Workout,
+      all: true
+    })
+
+    if (workouts !== null && 'error' in workouts) {
+      return res.status(503).json({
+        error: 'Database Error'
+      })
+    }
+
+    if (workouts.length === 0) {
+      return res.status(404).json({
+        message: `The trainings of day ${day} have not been found`
+      })
+    }
+
+    const withoutDayField = workouts.map(workout => {
+      const { day, ...data } = workout
+      return data
+    })
+
+    return res.json({
+      day,
+      workouts: withoutDayField
+    })
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    })
+  }
+}
+
 export const createWorkout = async (req, res) => {
   try {
     const { id } = req.session
