f42a3af884c66f1e75d82ed4bf8457d0862f80ce
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 18:00:47 -0300

    add numbers validator

--- src/utils/generalTools.js
+++ src/utils/generalTools.js
@@ -2,6 +2,10 @@ export const isString = (data) => {
   return typeof data === 'string'
 }
 
+export const isNumber = (data) => {
+  return !isNaN(Number(data))
+}
+
 export const getDate = () => {
   const date = new Date()
   const fullDate = date.toLocaleDateString()
