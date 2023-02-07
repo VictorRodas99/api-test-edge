d4f035777f9f1ceba212189105d907b206b0b0e5
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 18:02:55 -0300

    add function to create workout in db

---
+++ src/services/workouts.services.js
@@ -0,0 +1,27 @@
+import { Workout } from '../models/Workout.js'
+
+/**
+ * @param {{
+ * main_muscle_group: string,
+ * workout_name: string,
+ * day: number,
+ * series: number,
+ * repetitions: number }} data
+ *
+ * @returns { Promise<{ code: number, message: string }> } response
+ */
+export const createWorkout = async (data) => {
+  try {
+    await Workout.create(data)
+
+    return {
+      code: 200,
+      message: 'Workout created succesfully!'
+    }
+  } catch (error) {
+    return {
+      code: 503,
+      message: 'Database error'
+    }
+  }
+}
