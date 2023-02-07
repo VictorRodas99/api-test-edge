85918b15af29bcd7a5b8146118a8b9516940b8f2
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 19:47:48 -0300

    extract functions + add functionality to update specific fields

--- src/utils/validatorWorkout.js
+++ src/utils/validatorWorkout.js
@@ -1,35 +1,22 @@
-import { isString, isNumber } from './generalTools.js'
+import { validFieldsValidators, checkWorkoutFields, parseString, parseDay, parseNumber } from './workoutTools.js'
 
-const parseString = (dirt, field) => {
-  if (!isString(dirt)) {
-    throw new Error(`Invalid or missing ${field}`)
+export const validateWorkoutFields = (dirtyData, allFields = true) => {
+  if (!allFields) {
+    const data = {}
+    const { existsInvalidFields, givenFields } = checkWorkoutFields(dirtyData)
+
+    if (existsInvalidFields) {
+      throw new Error('There are invalid or missing fields!')
+    }
+
+    givenFields.forEach((field) => {
+      const validator = validFieldsValidators[field]
+      data[field] = validator(dirtyData[field])
+    })
+
+    return data
   }
 
-  return String(dirt)
-}
-
-export const parseDay = (dirtyDay) => {
-  if (!isNumber(dirtyDay)) {
-    throw new Error('Day field must be a number!')
-  }
-
-  const day = Number(dirtyDay)
-  if (day < 0 || day > 5) {
-    throw new Error('The given day must be between 1 and 5')
-  }
-
-  return day
-}
-
-const parseNumber = (dirtyNumber, field) => {
-  if (!isNumber(dirtyNumber)) {
-    throw new Error(`The ${field} field must be a number!`)
-  }
-
-  return Number(dirtyNumber)
-}
-
-export const validateWorkoutFields = (dirtyData) => {
   const data = {
     main_muscle_group: parseString(dirtyData.main_muscle_group, 'main muscle group'),
     workout_name: parseString(dirtyData.workout_name, 'workout name'),
