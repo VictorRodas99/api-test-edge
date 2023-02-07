6e22b6ec735f95d1c622114390c957cb31a67439
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 16:21:20 -0300

    add function to get all workouts

--- src/controllers/workouts.controllers.js
+++ src/controllers/workouts.controllers.js
@@ -1,5 +1,33 @@
-export const getWorkouts = () => {}
-export const getWorkoutById = () => {}
+import { Workout } from '../models/Workout.js'
+import { findBy } from '../services/tools.services.js'
+
+export const getWorkouts = async (req, res) => {
+  const { id } = req.session
+
+  const workouts = await findBy({
+    condition: { user_id: id },
+    model: Workout,
+    all: true
+  })
+
+  if (workouts !== null && 'error' in workouts) {
+    return res.status(503).json({
+      error: 'Database Error'
+    })
+  }
+
+  if (workouts.length === 0) {
+    return res.json({
+      message: 'You don\'t have routines, please create one'
+    })
+  }
+
+  return res.json({
+    totalWorkouts: workouts.length,
+    workouts
+  })
+}
+export const getWorkoutByDay = async (req, res) => {}
 export const createWorkout = () => {}
 export const updateWorkout = () => {}
 export const deleteWorkout = () => {}
