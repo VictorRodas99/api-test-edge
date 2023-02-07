9a20526a26f4597b8606f2aa5d677fd746d8c86c
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 15:09:51 -0300

    add functionality to register user to database

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,2 +1,35 @@
-export const registerUser = () => {}
-export const loginUser = () => {}
+import bcrypt from 'bcrypt'
+import { User } from '../models/User.js'
+import { findBy } from '../services/tools.services.js'
+import { createUser } from '../services/user.services.js'
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body // TODO: validate fields
+  const hashedPassword = await bcrypt.hash(password, 8)
+
+  const userExists = await findBy({ condition: { email }, model: User })
+
+  if (userExists != null && 'error' in userExists) {
+    return res.status(503).json({
+      error: 'Database Error'
+    })
+  }
+
+  if (userExists) {
+    return res.status(200).json({
+      message: 'Email already registered!'
+    })
+  }
+
+  const { code, message } = await createUser({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  return res.status(code).json({ message })
+}
+
+export const loginUser = async (_req, res) => {
+  res.json({ message: 'TODO' })
+}
