2acdf2023663074b1fba54f429a095dd2d0bf517
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-07 16:48:56 -0300

    add validations for user data in post

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,67 +3,80 @@ import { User } from '../models/User.js'
 import { findBy } from '../services/tools.services.js'
 import { createUser } from '../services/user.services.js'
 import { createToken } from '../utils/jwt.js'
+import { validateUserFields } from '../utils/validatorUser.js'
 
 export const registerUser = async (req, res) => {
-  const { name, email, password } = req.body // TODO: validate fields
-  const hashedPassword = await bcrypt.hash(password, 8)
+  try {
+    const { name, email, password } = validateUserFields(req.body)
+    const hashedPassword = await bcrypt.hash(password, 8)
 
-  const userExists = await findBy({ condition: { email }, model: User })
+    const userExists = await findBy({ condition: { email }, model: User })
 
-  if (userExists != null && 'error' in userExists) {
-    return res.status(503).json({
-      error: 'Database Error'
+    if (userExists != null && 'error' in userExists) {
+      return res.status(503).json({
+        error: 'Database Error'
+      })
+    }
+
+    if (userExists) {
+      return res.status(200).json({
+        message: 'Email already registered!'
+      })
+    }
+
+    const { code, message } = await createUser({
+      name,
+      email,
+      password: hashedPassword
+    })
+
+    return res.status(code).json({ message })
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message
     })
   }
-
-  if (userExists) {
-    return res.status(200).json({
-      message: 'Email already registered!'
-    })
-  }
-
-  const { code, message } = await createUser({
-    name,
-    email,
-    password: hashedPassword
-  })
-
-  return res.status(code).json({ message })
 }
 
 export const loginUser = async (req, res) => {
-  const { email, password } = req.body
-  const user = await findBy({ condition: { email }, model: User })
+  try {
+    const { email, password } = validateUserFields(req.body)
+    const user = await findBy({ condition: { email }, model: User })
 
-  if (!user) {
-    return res.status(404).json({
-      message: 'User not registered!'
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not registered!'
+      })
+    }
+
+    if ('error' in user) {
+      return res.status(503).json({
+        error: 'Database Error'
+      })
+    }
+
+    const dbPassword = user.password
+    const areEqual = await bcrypt.compare(password, dbPassword)
+
+    if (!areEqual) {
+      return res.status(400).json({
+        message: 'Wrong email and password combination!'
+      })
+    }
+
+    const userToken = createToken(user) // Create jsonwebtoken
+
+    res.cookie('access-token', userToken, {
+      maxAge: 2.592e9, // 30 days for this token
+      httpOnly: true
     })
-  }
 
-  if ('error' in user) {
-    return res.status(503).json({
-      error: 'Database Error'
+    return res.json({
+      message: `Welcome ${user.name}!`
     })
-  }
-
-  const dbPassword = user.password
-  const areEqual = await bcrypt.compare(password, dbPassword)
-
-  if (!areEqual) {
+  } catch (error) {
     return res.status(400).json({
-      message: 'Wrong email and password combination!'
+      error: error.message
     })
   }
-
-  const userToken = createToken(user) // Create jsonwebtoken
-
-  res.cookie('access-token', userToken, {
-    maxAge: 2.592e9, // 30 days for this token
-    httpOnly: true
-  })
-
-  return res.json({
-    message: `Welcome ${user.name}!`
-  })
 }
